0efb5b6778ce7e0079aa38dcd06cfa9a093630db
ultravert <feodor.sax@gmail.com>
2023-12-14 21:12:30 +0500

    migrations

--- database/migrations/2023_12_14_150224_create_users_table.php
+++ database/migrations/2023_12_14_150224_create_users_table.php
@@ -13,12 +13,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->timestamps();
+
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('avatar')->nullable();
+            $table->boolean('active')->default(true);
+
             $table->string('password');
             $table->rememberToken();
-            $table->timestamps();
         });
     }
 
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::resource('users', UserController::class);
+Route::resource('posts', PostController::class);
 
 Route::get('/', function () {
     return view('welcome');
